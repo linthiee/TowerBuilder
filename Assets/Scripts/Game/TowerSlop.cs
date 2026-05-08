808495e3c32335da9ec0bd90291d5a71f4443571
elu <eluujazmin@gmail.com>
2026-05-08 02:01:19 -0300

    add correctly all assets and fix chain collision with container bug

--- Assets/Scripts/Game/TowerSlop.cs
+++ Assets/Scripts/Game/TowerSlop.cs
@@ -7,10 +7,20 @@ public class TowerSlop : MonoBehaviour
 
     private void Update()
     {
+        if (slop.transform.parent != null)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        }
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             slop.useGravity = true;
             slop.transform.parent = null;
+
+            slop.isKinematic = false;
+
+            slop.linearVelocity = Vector3.zero;
+            slop.angularVelocity = Vector3.zero; 
         }
     }
 }
