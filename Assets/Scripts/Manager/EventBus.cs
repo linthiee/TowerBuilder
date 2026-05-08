c8706189d76b14b951b7f071d87ef3c49c4da1e0
elu <eluujazmin@gmail.com>
2026-05-08 11:44:03 -0300

    add eventBus and event bus interface

---
+++ Assets/Scripts/Manager/EventBus.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+public interface IEventBus
+{
+    void Subscribe<T>(Action<T> onEvent);
+    void Unsubscribe<T>(Action<T> onEvent);
+    void Publish<T>(T eventData);
+}
+
+public class EventBus : IEventBus
+{
+    private readonly Dictionary<Type, Delegate> _subscribers = new Dictionary<Type, Delegate>();
+    public void Subscribe<T>(Action<T> onEvent)
+    {
+        var type = typeof(T);
+        if (_subscribers.ContainsKey(type))
+            _subscribers[type] = Delegate.Combine(_subscribers[type], onEvent);
+        else
+            _subscribers[type] = onEvent;
+    }
+
+    public void Unsubscribe<T>(Action<T> onEvent)
+    {
+        var type = typeof(T);
+        if (_subscribers.ContainsKey(type))
+            _subscribers[type] = Delegate.Remove(_subscribers[type], onEvent);
+    }
+
+    public void Publish<T>(T eventData)
+    {
+        var type = typeof(T);
+        if (_subscribers.TryGetValue(type, out var action))
+            (action as Action<T>)?.Invoke(eventData);
+    }
+}
--- Assets/Scripts/Manager/ServiceLocator.cs
+++ Assets/Scripts/Manager/ServiceLocator.cs
@@ -8,37 +8,25 @@ public static class ServiceLoader
     public static void AddService<T>(T service)
     {
         if (!_services.TryAdd(typeof(T), service))
-        {
             _services.Add(typeof(T), service);
-        }
         else
-        {
             throw new Exception($"Service of type {typeof(T)} already exists");
-        }
     }
 
     public static T GetService<T>() where T : class
     {
         if (_services.TryGetValue(typeof(T), out var service))
-        {
             return (T)service;
-        }
         else
-        {
             throw new Exception($"Service of type {typeof(T)} not found");
-        }
     }
 
     public static void RemoveService<T>()
     {
         if (_services.ContainsKey(typeof(T)))
-        {
             _services.Remove(typeof(T));
-        }
         else
-        {
             throw new Exception($"Service of type {typeof(T)} not found");
-        }
     }
 
     public static void ClearServices()
