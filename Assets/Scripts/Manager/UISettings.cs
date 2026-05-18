7c727d1bf273fa06f24bfbd9d4b38c312dec6583
elu <eluujazmin@gmail.com>
2026-05-18 00:42:59 -0300

    add better menu and complete credits & settings

--- Assets/Scripts/Manager/UISettings.cs
+++ Assets/Scripts/Manager/UISettings.cs
@@ -4,52 +4,64 @@ using UnityEngine.UI;
 
 public class UISettings : MonoBehaviour
 {
-
     [SerializeField] private Slider sliderMaster;
     [SerializeField] private Slider sliderMusic;
     [SerializeField] private Slider sliderVFX;
 
     [SerializeField] private AudioSettingsSO settingsSO;
-
     [SerializeField] private AudioMixer mixer;
+
     private void Awake()
     {
         settingsSO.LoadSettings();
 
+        sliderMaster.onValueChanged.RemoveAllListeners();
+        sliderMusic.onValueChanged.RemoveAllListeners();
+        sliderVFX.onValueChanged.RemoveAllListeners();
+
         sliderMaster.value = settingsSO.masterVolume;
         sliderMusic.value = settingsSO.musicVolume;
         sliderVFX.value = settingsSO.vfxVolume;
-
-        mixer.SetFloat("MasterVolume", sliderMaster.value);
-        mixer.SetFloat("MusicVolume", sliderMusic.value);
-        mixer.SetFloat("VFXVolume", sliderVFX.value);
 
         sliderMaster.onValueChanged.AddListener(ChangeMasterVolume);
         sliderMusic.onValueChanged.AddListener(ChangeMusicVolume);
         sliderVFX.onValueChanged.AddListener(ChangeVFXVolume);
     }
 
-    private void OnDestroy()
+    private void Start()
     {
-        sliderMaster.onValueChanged.RemoveAllListeners();
-        sliderMusic.onValueChanged.RemoveAllListeners();
-        sliderVFX.onValueChanged.RemoveAllListeners();
+        mixer.SetFloat("MasterVolume", ConvertToDecibels(sliderMaster.value));
+        mixer.SetFloat("MusicVolume", ConvertToDecibels(sliderMusic.value));
+        mixer.SetFloat("VFXVolume", ConvertToDecibels(sliderVFX.value));
     }
+
     private void ChangeMasterVolume(float currentValue)
     {
         settingsSO.SaveMaster(currentValue);
-        mixer.SetFloat("MasterVolume", currentValue);
+        mixer.SetFloat("MasterVolume", ConvertToDecibels(currentValue));
     }
 
     private void ChangeMusicVolume(float currentValue)
     {
         settingsSO.SaveMusic(currentValue);
-        mixer.SetFloat("MusicVolume", currentValue);
+        mixer.SetFloat("MusicVolume", ConvertToDecibels(currentValue));
     }
 
     private void ChangeVFXVolume(float currentValue)
     {
         settingsSO.SaveVFX(currentValue);
-        mixer.SetFloat("VFXVolume", currentValue);
+        mixer.SetFloat("VFXVolume", ConvertToDecibels(currentValue));
+    }
+
+    private float ConvertToDecibels(float linearValue)
+    {
+        if (linearValue <= 0.0001f)
+        {
+            return -80f;
+        }
+        else
+        {
+            return Mathf.Log10(linearValue) * 20;
+        }
     }
 }
